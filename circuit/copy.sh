97902fab4a49f11f70e5059bf31039972f2bb691
xumingkuan <xumingkuan0721@126.com>
2024-03-29 21:43:25 -0400

    copy circuits

---
+++ circuit/copy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the array of strings
+strings=("ising" "qsvm" "vqc" "bv")
+n_values=(28 29 30 31 32 33 34)
+
+# Iterate over the strings
+for str in "${strings[@]}"; do
+    # Iterate over the n values
+    for n in "${n_values[@]}"; do
+        # Source and destination file paths
+        src_file="NWQBench/${str}_n${n}_no_swap.qasm"
+        dest_file="MQTBench_${n}q/${str}_indep_qiskit_${n}_no_swap.qasm"
+        
+        # Copy the file
+        cp $src_file $dest_file
+    done
+done
